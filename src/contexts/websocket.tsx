0adcec277289216ea904ad46de966b201988e9c4
QIMINFO\dfe <daniel.febrero@qiminfo.ch>
2021-09-28 12:03:55 +0200

    Create websocket context

---
+++ src/contexts/websocket.tsx
@@ -0,0 +1,43 @@
+import React, { createContext, useState, useEffect } from 'react';
+import { HubConnectionBuilder, LogLevel, HubConnection } from '@microsoft/signalr';
+
+const WebSocketContext = createContext(null)
+
+export { WebSocketContext }
+
+export default ({ children }) => {
+    const [connection, setConnection] = useState<HubConnection>();
+    const [connecting, setConnecting] = useState<boolean>(false);
+
+
+    useEffect(() => {
+        const connect = async () => {
+            try {
+                setConnecting(true);
+        
+                const connection = new HubConnectionBuilder()
+                .withUrl("https://localhost:44333/atms")
+                .configureLogging(LogLevel.Information)
+                .build();
+        
+                connection.onclose(e => {
+                    setConnection(null);
+                });
+        
+                await connection.start();
+                setConnection(connection);
+                setConnecting(false);
+            } catch (e) {
+                console.log(e);
+            }
+        }
+
+        !connection && connect();
+    });
+
+    return (
+        <WebSocketContext.Provider value={[connection, connecting]}>
+            {children}
+        </WebSocketContext.Provider>
+    )
+}
